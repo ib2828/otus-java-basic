51b9eb400b93cbc9c91af888eae68c6c5ecf2e4e
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-05 18:17:15 +0300

    add homework4

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/Application.java
@@ -0,0 +1,83 @@
+package ru.otus.java.basic.homeworks.homework4;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Лапшин", "Артемий", "Алексеевич", 1980, "lapshin@mail.ru"),
+                new User("Семенова", "Милана", "Николаевна", 1992, "milana@mail.ru"),
+                new User("Борисова", "Анна", "Руслановна", 2001, "anna@mail.ru"),
+                new User("Копылова", "Елизавета", "Ивановна", 2005, "liza@mail.ru"),
+                new User("Полякова", "Екатерина", "Ивановна", 1979, "katy@mail.ru"),
+                new User("Зайцев", "Александр", "Ярославович", 1995, "sasha@mail.ru"),
+                new User("Иванова", "Алена", "Львовна", 1987, "alena@mail.ru"),
+                new User("Овчинникова", "Александра", "Максимовна", 1990, "sash2@mail.ru"),
+                new User("Шестаков", "Максим", "Фёдорович", 1988, "max@mail.ru"),
+                new User("Добрынина", "Алиса", "Леонидовна", 1973, "alisa@mail.ru"),
+        };
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getAge() > 40) {
+                users[i].getUserInfo();
+            }
+        }
+        System.out.println("##########################################################################################");
+        System.out.println("##########################################################################################");
+        System.out.println("##########################################################################################");
+        boolean nextLoop = true;
+        Box box1 = new Box(1, 2, 4, "White", true, false);
+        Scanner scanner = new Scanner(System.in);
+        Scanner stringScanner = new Scanner(System.in);
+
+        do {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Введите номер необходимого действия");
+            System.out.println("1 Вывод информации о коробке");
+            System.out.println("2 Изменение цвета коробки");
+            System.out.println("3 Открыть коробку");
+            System.out.println("4 Закрыть коробку");
+            System.out.println("5 Положить предмет в коробку");
+            System.out.println("6 Извлечь предмет из коробки");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            int numberMethod = scanner.nextInt();
+            switch (numberMethod) {
+                case 1:
+                    System.out.println("Информация о коробке");
+                    box1.getInfo();
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Введите нужный цвет коробки");
+                    String color = stringScanner.next();
+                    box1.setColor(color);
+                    System.out.println();
+                    break;
+                case 3:
+                    box1.open();
+                    break;
+                case 4:
+                    box1.close();
+                    break;
+                case 5:
+                    box1.putItem();
+                    break;
+                case 6:
+                    box1.getItem();
+                    break;
+                case 0:
+                    System.out.println("Спасибо! Вы закончили работу");
+                    nextLoop = false;
+                    break;
+                default:
+                    System.out.println("Спасибо! Вы выбрали несуществующий метод ");
+                    break;
+            }
+        } while (nextLoop);
+
+
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/Box.java
@@ -0,0 +1,79 @@
+package ru.otus.java.basic.homeworks.homework4;
+
+public class Box {
+    private int length;
+    private int width;
+    private int height;
+    private String color;
+    private boolean isOpened;
+    private boolean isItem;
+
+    public Box(int length, int width, int height, String color, boolean isOpened, boolean isItem) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.isOpened = isOpened;
+        this.isItem = isItem;
+
+    }
+
+    public void getInfo() {
+        System.out.println("Длина коробки " + length);
+        System.out.println("Ширина коробки " + width);
+        System.out.println("Высота коробки " + height);
+        System.out.println("Цвет коробки " + color);
+        System.out.println("Коробка открыта? " + isOpened);
+        System.out.println();
+    }
+
+    public void putItem() {
+        if (this.isOpened) {
+            if (this.isItem) {
+                System.out.println("Не удалось положить в коробку предмет. В коробке уже есть предмет");
+            } else {
+                this.isItem = true;
+                System.out.println("Предмет положен в коробку");
+            }
+        } else {
+            System.out.println("Коробка закрыта");
+        }
+
+    }
+
+    public void getItem() {
+        if (this.isOpened) {
+            if (this.isItem) {
+                this.isItem = false;
+                System.out.println("Предмет извлечен из коробки");
+            } else {
+                System.out.println("В коробке нету предмета");
+            }
+        } else {
+            System.out.println("Коробка закрыта");
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+        System.out.println("Цвет коробки изменен на " + color);
+    }
+
+    public boolean isOpened() {
+        return isOpened;
+    }
+
+    public void open() {
+        System.out.println("Коробка открыта");
+        isOpened = true;
+    }
+
+    public void close() {
+        System.out.println("Коробка закрыта");
+        isOpened = false;
+    }
+}
