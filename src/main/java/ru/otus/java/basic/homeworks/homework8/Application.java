674cbb82b3ca40ea1f070f86bf5723ef42c7c30c
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-21 21:40:27 +0300

    intermediate code

--- src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
@@ -4,10 +4,27 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        String [][] array = {{"1","1"},{"2","2"}};;
+        String[][] array = {{"1", "a", "1"}, {"2", "2","2"}, {"3","3", "3"}};
+        System.out.println(arrayProcessing(array));
+    }
 
+    public static int arrayProcessing(String[][] a) {
+        int s = 0;
+        try {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    try {
+                        s += Integer.parseInt(a[i][j]);
+                    } catch (NumberFormatException e){
+                        System.out.println("Ошибка преобразовании элемента в строке " + i + " столбце " + j);
+                    }
+                }
+            }
+        return s;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка в размере массива");
+            return 0;
+        }
     }
-    public static void arrayProcessing(){
-        System.out.println();
-    }
+
 }
