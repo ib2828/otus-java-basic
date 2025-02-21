7bd7579bfacee2b1e68915a6e9923e5aaffcc631
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-21 21:05:03 +0300

    hw08 init

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
@@ -0,0 +1,13 @@
+package ru.otus.java.basic.homeworks.homework8;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        String [][] array = {{"1","1"},{"2","2"}};;
+
+    }
+    public static void arrayProcessing(){
+        System.out.println();
+    }
+}
