5bf812c3bed4987a5754628b0201b1498e9a6557
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-24 18:47:38 +0300

    hw08 success homework

--- src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
@@ -2,32 +2,28 @@ package ru.otus.java.basic.homeworks.homework8;
 
 public class Application {
     public static void main(String[] args) {
-        int sizeArray = 3;
-        String[][] array = {{"1", "1", "1"}, {"2", "2", "2"}, {"3", "0", "3"}};
+        int sizeArray = 4;
+        String[][] array = {{"1", "1", "1", "1"}, {"2", "2", "2", "2"}, {"3", "3", "3", "3"}, {"4", "4", "4", "4"}};
         try {
             System.out.println(arrayProcessing(array, sizeArray));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("На вход функции передан некорректный массив");
+        } catch (AppArraySizeException | AppArrayDataException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public static int arrayProcessing(String[][] a, int size) {
+    public static int arrayProcessing(String[][] a, int size) throws AppArraySizeException, AppArrayDataException {
+        if ( a.length < size ) throw new AppArraySizeException("Неправильный размер массива");
+
         int s = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                s += stringToInteger(a[i][j], i, j);
+                try {
+                    s += Integer.parseInt(a[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new AppArrayDataException(String.format("Элемент: \"%s\" не удалось преобразовать в тип Integer в строке: %s колонке %s", a[i][j], i+1, j+1));
+                }
             }
         }
         return s;
-    }
-
-    public static int stringToInteger(String z, int row, int column)  throws AppArraySizeException{
-        try {
-            int a = Integer.parseInt(z);
-            return a;
-        } catch (AppArraySizeException e) {
-            System.out.println("Ошибка преобразовании элемента в строке " + row + " столбце " + column);
-            return 0;
-        }
     }
 }
