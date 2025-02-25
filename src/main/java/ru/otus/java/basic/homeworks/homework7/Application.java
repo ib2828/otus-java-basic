8ecf478fe4241581084a3fe6263d725a8358f974
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-25 15:42:51 +0300

    hw07 add enum Terrain

--- src/main/java/ru/otus/java/basic/homeworks/homework7/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework7/Application.java
@@ -2,6 +2,7 @@ package ru.otus.java.basic.homeworks.homework7;
 
 public class Application {
     public static void main(String[] args) {
+//        Terrain terrain = Terrain.FOREST;
         System.out.println();
     }
 }
