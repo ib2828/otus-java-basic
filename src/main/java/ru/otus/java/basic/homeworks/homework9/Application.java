10e481c49e7a74d4ee934db382d94b2d42fbe07f
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-26 12:02:00 +0300

    hw09 add method sumElement

--- src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
@@ -4,16 +4,36 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        System.out.println(filingList(1,20));
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("3");
+        list.add("6");
+        list.add("5");
+        list.add("5");
+        list.add("10");
+        System.out.println("Array list последовательных элементов");
+        System.out.println(filingList(1, 20));
+        System.out.println("Сумма элементов, значение которых больше 5");
+        System.out.println(sumElements(list));
     }
 
-    public static ArrayList filingList(int minValue, int maxValue){
-        ArrayList<Integer> listLocal = new ArrayList<Integer>();
+    public static ArrayList filingList(int minValue, int maxValue) {
+        ArrayList<Integer> listLocal = new ArrayList();
 
-        for (int i = minValue; i < maxValue +1; i++) {
+        for (int i = minValue; i < maxValue + 1; i++) {
             listLocal.add(i);
         }
         return listLocal;
+    }
+
+    public static int sumElements(ArrayList list) {
+        int s = 0;
+        for (int i =0; i < list.size(); i++) {
+            int a = Integer.parseInt((String) list.get(i));
+            if ( a > 5) {
+                s += a;
+            }
+        }
+        return s;
     }
 }
