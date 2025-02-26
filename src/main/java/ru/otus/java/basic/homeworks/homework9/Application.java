70b63137337ff566b48908dece67a01643e9372c
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-26 16:44:01 +0300

    hw09 add method numberIncrease

--- src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
@@ -4,24 +4,23 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("3");
-        list.add("6");
-        list.add("5");
-        list.add("5");
-        list.add("10");
+        ArrayList<Integer> elements = new ArrayList<>();
+        elements.add(3);
+        elements.add(5);
+        elements.add(7);
+        elements.add(9);
         System.out.println("Array list последовательных элементов");
         System.out.println(filingList(1, 20));
         System.out.println("Сумма элементов, значение которых больше 5");
-        System.out.println(sumElements(list));
+        System.out.println(sumElements(elements));
         System.out.println("Замена всех элементов одним числом");
-        System.out.println(replaceNumber(list, 9));
+        System.out.println(replaceNumber(elements, 9));
+        System.out.println("Замена всех элементов одним числом");
+        System.out.println(numberIncrease(elements, 9));
     }
 
     public static ArrayList filingList(int minValue, int maxValue) {
         ArrayList<Integer> listLocal = new ArrayList();
-
         for (int i = minValue; i < maxValue + 1; i++) {
             listLocal.add(i);
         }
@@ -31,7 +30,7 @@ public class Application {
     public static int sumElements(ArrayList list) {
         int s = 0;
         for (int i =0; i < list.size(); i++) {
-            int a = Integer.parseInt((String) list.get(i));
+            int a = (int) list.get(i);
             if ( a > 5) {
                 s += a;
             }
@@ -40,7 +39,14 @@ public class Application {
     }
     public static ArrayList replaceNumber(ArrayList list, int x) {
         for (int i =0; i < list.size(); i++) {
-            list.set(i, Integer.toString(x));
+            list.set(i, x);
+        }
+        return list;
+    }
+    public static ArrayList numberIncrease(ArrayList list, int x) {
+        for (int i =0; i < list.size(); i++) {
+            int a = (int) list.get(i) + x;
+            list.set(i, a);
         }
         return list;
     }
