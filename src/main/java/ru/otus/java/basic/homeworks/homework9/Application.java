278c35357a08914a4569fdaa3a5b7ef1c4ef7089
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-02-26 17:29:29 +0300

    hw09 add method getEmployeesName

--- src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static Employee Bob = new Employee("Bob", 20);
+    private static Employee Jon = new Employee("Jon", 30);
+    private static Employee Nick = new Employee("Nick", 40);
+    private static Employee Mike = new Employee("Mike", 50);
+
     public static void main(String[] args) {
         ArrayList<Integer> elements = new ArrayList<>();
         elements.add(3);
         elements.add(5);
         elements.add(7);
         elements.add(9);
+
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        employees.add(Bob);
+        employees.add(Jon);
+        employees.add(Nick);
+        employees.add(Mike);
+
         System.out.println("Array list последовательных элементов");
         System.out.println(filingList(1, 20));
         System.out.println("Сумма элементов, значение которых больше 5");
@@ -17,7 +29,9 @@ public class Application {
         System.out.println("Замена всех элементов одним числом");
         System.out.println(numberReplace(elements, 9));
         System.out.println("Увеличение каждого элемента массива");
-        System.out.println(numberIncrease(elements, 9));
+        System.out.println(numberIncrease(elements, 10));
+        System.out.println("Список имен сотрудников");
+        System.out.println(getEmployeesName(employees));
     }
 
     public static ArrayList filingList(int minValue, int maxValue) {
@@ -46,11 +60,19 @@ public class Application {
         return list;
     }
 
-    public static List numberIncrease(List list, int x) {
+    public static List numberIncrease(List<Integer> list, int x) {
         for (int i = 0; i < list.size(); i++) {
-            int a = (int) list.get(i) + x;
+            int a = list.get(i) + x;
             list.set(i, a);
         }
         return list;
+    }
+
+    public static List getEmployeesName(ArrayList<Employee> employees){
+        ArrayList<String> names = new ArrayList<String>();
+        for (int i = 0; i < employees.size(); i++) {
+            names.add(employees.get(i).getName());
+        }
+        return names;
     }
 }
