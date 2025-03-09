940f79c61cac2f598d554f0c180fb0e6ee368d0a
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-03-09 06:22:59 +0300

    hw6 fix

--- src/main/java/ru/otus/java/basic/homeworks/homework6/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework6/Application.java
@@ -3,8 +3,6 @@ package ru.otus.java.basic.homeworks.homework6;
 public class Application {
     public static void main(String[] args) {
 
-        // 7. Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки
-        // и потом вывести информацию о сытости котов в консоль.
         Cat[] cats = {
                 new Cat("Барсик", 12),
                 new Cat("Мурзик", 7),
