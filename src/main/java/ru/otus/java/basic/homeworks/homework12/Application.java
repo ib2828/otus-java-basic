1f37593e34566cd26963c8086601c5421b7d06e0
igor batomunkuev <batomunkuev.ig@gmail.com>
2025-04-20 16:33:22 +0300

    hw12 init
    hw12

--- src/main/java/ru/otus/java/basic/homeworks/homework12/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework12/Application.java
@@ -2,28 +2,19 @@ package ru.otus.java.basic.homeworks.homework12;
 
 import java.io.*;
 
-
 public class Application {
     public static void main(String[] args) {
-        listing_dir();
-    }
 
-    public static void listing_dir() {
-        File dir = new File("./");
-        FilenameFilter txtFilter = new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().endsWith(".txt");
-            }
-        };
+        String pathProject = System.getProperty("user.dir") + "/";
+        String extension = ".txt";
 
-        File[] filesList = dir.listFiles(txtFilter);
+        WorkFiles workDir = new WorkFiles(pathProject, extension);
 
-        for (File file : filesList) {
-            if (file.isFile()) {
-                System.out.println(file.getName());
-            }
-
-        }
+        File[] files = workDir.GettingListFiles(pathProject, extension);
+        String fileName = UserChoice.SelectFile(files);
+        workDir.CustomFileReader(fileName, pathProject);
+        workDir.NewLineWriter(fileName, pathProject);
+        workDir.CustomFileReader(fileName, pathProject);
     }
 }
 
